46e4070b27e683aadb831c464a2a4baaff67df38
Swapnil Mall <swapnilmall03@gmail.com>
2020-02-10 13:00:09 +0530

    removed the if else

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,10 @@ import Login from "./login";
 
 var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
-
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
 }
 
 export default App;
